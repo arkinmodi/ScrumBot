68147ab350fcf9116e9759851ab841120d1dadac
Leon So <leon_so7@hotmail.com>
2020-02-12 18:04:34 -0500

    changes to meeting module

--- ProofOfConcept/Meeting.py
+++ ProofOfConcept/Meeting.py
@@ -7,9 +7,10 @@ class Meeting:
         time = list(map(int, time.split(':')))
         self._date_time = datetime(date[2], date[0], date[1], time[0], time[1])
         self._meeting_type = meeting_type
+        print self._date_time.strftime("(" + self._meeting_type + ")" + " Meeting at: %b %d, %Y at %I:%M%p"))
 
     def get_date_time(self) -> str:
-        return self._date_time.strftime("Meeting scheduled for: %b %d, %Y at %I:%M%p")
+        return self._date_time.strftime("Meeting at: %b %d, %Y at %I:%M%p")
 
     def get_meeting_type(self) -> str:
-        return self._meeting_type
+        return ("Type: " + self._meeting_type)
