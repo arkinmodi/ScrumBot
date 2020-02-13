af6f56cda62ad6a346f1380cdc458e9b30fc5e64
Leon So <leon_so7@hotmail.com>
2020-02-13 12:06:17 -0500

    Meeting list ADT

--- ProofOfConcept/MeetingList.py
+++ ProofOfConcept/MeetingList.py
@@ -6,11 +6,12 @@ class MeetingList:
         self._idCount = 0
 
     def add_meeting(self, meeting):
-        self._meeting_list[self._idCount] = Meeting
+        self._meeting_list[self._idCount] = meeting
+        self._idCount += 1
     
     def list_meetings(self):
         sortedList = []
         for key in sorted(self._meeting_list.keys()) :
             meeting_str = "(" + str(key) + ")" + self._meeting_list[key].get_meeting_type() + " Meeting - " +  self._meeting_list[key].get_date_time()
-            sortedList.push()
+            sortedList.append(meeting_str)
         return sortedList
