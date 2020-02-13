870ab5cde18942078b6741372a97811bddd24240
Leon So <leon_so7@hotmail.com>
2020-02-13 12:08:00 -0500

    fix naming

--- ProofOfConcept/MeetingList.py
+++ ProofOfConcept/MeetingList.py
@@ -10,8 +10,11 @@ class MeetingList:
         self._idCount += 1
     
     def list_meetings(self):
-        sortedList = []
+        sorted_list = []
         for key in sorted(self._meeting_list.keys()) :
             meeting_str = "(" + str(key) + ")" + self._meeting_list[key].get_meeting_type() + " Meeting - " +  self._meeting_list[key].get_date_time()
-            sortedList.append(meeting_str)
-        return sortedList
+            sorted_list.append(meeting_str)
+        return sorted_list
+
+    # def remove_meeting(self):
+
