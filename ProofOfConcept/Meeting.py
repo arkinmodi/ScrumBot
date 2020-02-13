fbb752fca993db5363bf71fadf2c9e177600db72
Arkin Modi <arkinmodi@gmail.com>
2020-02-12 21:28:32 -0500

    Added missing bracket

--- ProofOfConcept/Meeting.py
+++ ProofOfConcept/Meeting.py
@@ -7,7 +7,7 @@ class Meeting:
         time = list(map(int, time.split(':')))
         self._date_time = datetime(date[2], date[0], date[1], time[0], time[1])
         self._meeting_type = meeting_type
-        print self._date_time.strftime("(" + self._meeting_type + ")" + " Meeting at: %b %d, %Y at %I:%M%p"))
+        print(self._date_time.strftime("(" + self._meeting_type + ")" + " Meeting at: %b %d, %Y at %I:%M%p"))
 
     def get_date_time(self) -> str:
         return self._date_time.strftime("Meeting at: %b %d, %Y at %I:%M%p")
