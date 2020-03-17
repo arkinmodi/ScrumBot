fe4991923748b49df68da3a828d1732ffc7f637a
Leon So <leon_so7@hotmail.com>
2020-03-17 14:53:54 -0400

    Updates to task module

--- src/task.py
+++ src/task.py
@@ -3,15 +3,51 @@
 #  @brief A class representing tasks during the software development cycle
 #  @date Mar 17, 2020
 
+## @brief Class representing tasks
+#  @details Class representing tasks with fields name, deadline, details and feedback
 class Task():
-    def __init__(s, dt):
+
+    ## @brief Constructor for Task object
+    def __init__(self, s, dt):
         self.name = s
         self.deadline = dt
         self.details = None
+        self.feedback = []
 
-    def __init__(s, dt, d):
+    ## @brief Constructor for Task object
+    def __init__(self, s, dt, d):
         self.name = s
         self.deadline = dt
         self.details = d
+        self.feedback = []
+
+    ## @brief Accessor for deadline of task
+    def get_deadline(self):
+        return self.deadline
+
+    ## @brief Accessor for details of task
+    def get_details(self):
+        if (self.details == None):
+            return "No details"
+        else:
+            return self.details
+
+     ## @brief Accessor for feedback of task
+    def get_feedback(self):
+        return self.feedback
+
+     ## @brief Mutator for adding new feedback to task
+    def add_feedback(self, s):
+        return self.feedback.append(s)
+
+     ## @brief Mutator for removing feedback from task
+    def rm_feedback(self, s):
+        return self.feedback.remove(s)
+
+     ## @brief Mutator for setting details of a task
+    def set_details(self, s):
+        self.details = s
+
+    
 
     
