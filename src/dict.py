c15e40cbd893a4f281945a61921dfa109628426f
Leon So <leon_so7@hotmail.com>
2020-03-17 14:12:00 -0400

    Add Generic Dict Module

---
+++ src/dict.py
@@ -0,0 +1,35 @@
+## @file dict.py
+#  @author Arkin Modi, Leon So, Timothy Choy
+#  @brief Custom dictionary ADT
+#  @date Mar 17, 2020
+
+
+## @brief Generic Dict ADT
+#  @details A generic dictionary ADT module which had add, remove, and to_seq methods
+class Dict():
+    
+    ## @brief Dict constructor
+    def __init__(self):
+        self.d = {}
+        self.c = 0
+
+    ## @brief Adds a new entry
+    #  @param e New entry to be added to the Dict
+    def add(self, e):
+        self.d[c] = e
+        self.c += 1
+
+    ## @brief Remove an entry with key id 
+    #  @param id Key of the entry
+    #  @throws KeyError If ID does not exist
+    def remove(id):
+        if id in self.d:
+            del self.d[id]
+        else:
+            raise KeyError
+    
+    ## @brief Returns a dictionary with keys in sorted order
+    def to_seq():
+        return sorted(wordsFreqDict.keys())
+    
+
