068cfa3923faff6764da2d4dac8236be9ff95ba4
Leon So <leon_so7@hotmail.com>
2020-03-17 14:42:02 -0400

    Add Task module

---
+++ src/task.py
@@ -0,0 +1,17 @@
+## @file task.py
+#  @author Arkin Modi, Leon So, Timothy Choy
+#  @brief A class representing tasks during the software development cycle
+#  @date Mar 17, 2020
+
+class Task():
+    def __init__(s, dt):
+        self.name = s
+        self.deadline = dt
+        self.details = None
+
+    def __init__(s, dt, d):
+        self.name = s
+        self.deadline = dt
+        self.details = d
+
+    
