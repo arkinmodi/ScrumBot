bf4450f15fca907a692ab6876fdba14723747f9c
Leon So <leon_so7@hotmail.com>
2020-03-17 21:02:37 -0400

    Project List

---
+++ src/projectList.py
@@ -0,0 +1,8 @@
+## @file projectList.py
+#  @author Arkin Modi, Leon So, Timothy Choy
+#  @brief Dict of Projects
+#  @date Mar 17, 2020
+
+## @brief Dict of Projects
+class ProjectList(Dict):
+    pass
