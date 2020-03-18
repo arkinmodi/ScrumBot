d992e6c7f4fd200139d5dedb6e75acf5d7ad672b
Leon So <leon_so7@hotmail.com>
2020-03-17 20:37:37 -0400

    Meeting List module

---
+++ src/meetingList.py
@@ -0,0 +1,8 @@
+## @file meetingList.py
+#  @author Arkin Modi, Leon So, Timothy Choy
+#  @brief Dict of Meetings
+#  @date Mar 17, 2020
+
+## @brief Dict of Meetings
+class MeetingList(Dict):
+    pass
