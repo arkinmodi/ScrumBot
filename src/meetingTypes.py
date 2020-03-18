f3a540d976dfbd3668dd5cc5db537e9a88d79d99
Leon So <leon_so7@hotmail.com>
2020-03-17 20:25:25 -0400

    Meeting Types module

---
+++ src/meetingTypes.py
@@ -0,0 +1,13 @@
+## @file meetingTypes.py
+#  @author Arkin Modi, Leon So, Timothy Choy
+#  @brief Class with enumeration of meeting types
+#  @date Mar 17, 2020
+
+from enum import Enum
+
+## @brief Class containing enumeration of meeting types
+class MeetingTypes():
+        GROOMING = auto()
+        STANDUP = auto()
+        RETROSPECTIVE = auto()
+        SPRINTPLANNING = auto()
