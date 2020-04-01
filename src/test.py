d31aa6e63f7acb285c7bf91d1732cbcfb516f2ed
Leon So <leon_so7@hotmail.com>
2020-04-01 16:45:10 -0400

    Add doxygen comments

--- src/test.py
+++ src/test.py
@@ -6,23 +6,25 @@
 import pytest
 import project
 
-# Test Plan Section 3.1.1 Installation
+## @brief Test Plan Section 3.1.1 Installation
 class Test_FR_Installation:
     # All Manual Testing
     pass
 
-# Test Plan Section 3.1.2 Project Creation
+## @brief Test Plan Section 3.1.2 Project Creation
 class Test_FR_ProjectCreation:
+    ## @brief Test case for project creation
     def test_create_project(self):
         test_project = project.Project("Test Name", "Test Description")
         assert( test_project.get_name() == "Test Name" and 
                 test_project.get_desc() == "Test Description")
 
+    ## @brief Test case for project creation without description
     def test_no_description(self):
         test_project = project.Project("Test Name")
         assert( test_project.get_name() == "Test Name" and 
                 test_project.get_desc() == "No description")
 
-# Test Plan Section 3.1.3 Project Removal
+## @brief Test Plan Section 3.1.3 Project Removal
 class Test_FR_ProjectRemoval:
     pass
